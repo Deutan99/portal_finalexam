b7d69825f4e9ef914d560626bf599cf1fc44e6d6
HEO JAE HYEOK <41223338+Deutan99@users.noreply.github.com>
2023-06-15 01:07:42 +0900

    깃 연동

---
+++ src/main/java/com/boot/portal_final/PortalFinalApplication.java
@@ -0,0 +1,14 @@
+package com.boot.portal_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalFinalApplication.class, args);
+		System.out.println("Hello Spring boot !!!");
+	}
+
+}
---
+++ src/test/java/com/boot/portal_final/PortalFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.portal_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
